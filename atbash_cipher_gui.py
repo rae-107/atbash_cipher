252e56aba7c799bd33dce0310b0a8a4cd7df7a4d
Rae Gebhart <raegebhart@gmail.com>
2023-07-13 10:50:15 -0600

    Initialize tkinter

---
+++ atbash_cipher_gui.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+class AtbashCipherGUI:
+  def __init__(self):
+    self.window = tk.Tk()
+
+    self.window.geometry('700x550')
+    self.window.title("Atbash Cipher")
+
+    self.window.mainloop()
+
+
+AtbashCipherGUI()
