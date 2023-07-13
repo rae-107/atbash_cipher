4ae775f3d16521d303f09b9347e66c734b807e25
Rae Gebhart <raegebhart@gmail.com>
2023-07-13 10:40:15 -0600

    Add proper UI to program

--- atbash_cipher_gui.py
+++ atbash_cipher_gui.py
@@ -11,6 +11,41 @@ class MyGUI:
     self.window.geometry('700x550')
     self.window.title("Atbash Cipher")
 
+    self.welcome = tk.Label(self.window, text='Welcome', font=('Arial', 34))
+    self.welcome.pack(pady=20)
+
+    self.get_started = tk.Label(self.window, text='Get Started By Typing In A Message', font=('Arial', 18))
+    self.get_started.pack()  
+
+    self.message = tk.StringVar()
+
+    self.user_input = tk.Entry(self.window, font=('Arial', 20), textvariable=self.message)
+    self.user_input.pack(pady=20)
+
+    self.buttonframe = tk.Frame(self.window)
+    self.buttonframe.columnconfigure(0, weight=1)
+    self.buttonframe.columnconfigure(1, weight=1)
+
+    self.encode_button = tk.Button(self.buttonframe, text='Encode Message', font=('Arial', 14),command=self.encode_handler)
+    self.encode_button.grid(row=0, column=0)
+
+    self.decode_button = tk.Button(self.buttonframe, text='Decode Message', font=('Arial', 14), command=self.decode_handler)
+    self.decode_button.grid(row=0, column=1)
+
+    self.buttonframe.pack()
+
+    self.original_message_label = tk.Label(self.window, font=('Arial', 14))
+    self.original_message_label.pack()
+
+    self.original_message = tk.Label(self.window, font=('Arial', 18))
+    self.original_message.pack(pady=10)
+
+    self.output_label = tk.Label(self.window, font=('Arial', 14))
+    self.output_label.pack()
+
+    self.output = tk.Label(self.window, font=('Arial', 18))
+    self.output.pack(pady=10)
+
     self.window.mainloop()
 
   def encode_message(self, phrase):
